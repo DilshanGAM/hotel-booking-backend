0abad5188a06e3a842df64df338750f75c5ffd1a
Dilshan G.A.M <93402921+DilshanGAM@users.noreply.github.com>
2024-11-23 13:17:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.use("/api/category",categoryRouter)
 app.use("/api/rooms",roomRouter)
 app.use("/api/bookings",bookingRouter)
 
-app.listen(5000,(req,res)=>{
-  console.log("Sever is running on on port 5000")
+app.listen(3000,(req,res)=>{
+  console.log("Sever is running on on port 3000")
 });
 
